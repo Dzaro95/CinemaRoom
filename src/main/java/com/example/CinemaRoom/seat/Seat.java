62bbd923eac2bdd00ba22ae1dc2a6e85ce2bb075
Dzaro95 <jarek-marciniak7@wp.pl>
2024-01-12 02:38:57 +0100

    refactoring

--- src/main/java/com/example/CinemaRoom/seat/Seat.java
+++ src/main/java/com/example/CinemaRoom/seat/Seat.java
@@ -5,9 +5,6 @@ public class Seat {
     private int column;
     private int price;
 
-    public Seat() {
-    }
-
     public Seat(int row, int column) {
         if(SeatsService.ROWS < row || SeatsService.COLUMNS < column) {
             throw new PurchaseException("The number of a row or a column is out of bounds!");
@@ -22,26 +19,13 @@ public class Seat {
     public int getPrice() {
         return price;
     }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
     public int getRow() {
         return row;
     }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
     public int getColumn() {
         return column;
     }
 
-    public void setColumn(int column) {
-        this.column = column;
-    }
 }
 
 
