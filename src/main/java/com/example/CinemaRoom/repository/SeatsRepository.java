277072aacd506922527b8c1a6d17d7ba4b84ab33
Dzaro95 <jarek-marciniak7@wp.pl>
2024-01-17 00:54:46 +0100

    refactoring

--- src/main/java/com/example/CinemaRoom/repository/SeatsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.CinemaRoom.repository;
-/*
-import com.example.CinemaRoom.model.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatsRepository extends JpaRepository<Seat, Long> {
-}
-
- */
