217be91daff594fb9c055a3376212b2c573756e0
Dzaro95 <jarek-marciniak7@wp.pl>
2024-01-20 13:12:27 +0100

    remove line

--- src/main/java/com/example/CinemaRoom/controller/MovieController.java
+++ src/main/java/com/example/CinemaRoom/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovieController {
+
     private final SeatsService seatsService = new SeatsService();
     private final StatisticsService statisticsService = new StatisticsService(seatsService.numberOfSeats());
     private final PurchaseService purchaseService = new PurchaseService(statisticsService);
