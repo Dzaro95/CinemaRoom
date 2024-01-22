68054e22e2c7d6aa3444cbcff0bd677877bfa5cc
Dzaro95 <jarek-marciniak7@wp.pl>
2024-01-22 21:31:28 +0100

    merge refactor

--- src/main/java/com/example/CinemaRoom/exception/StatisticsInfoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.CinemaRoom.exception;
-
-public class StatisticsInfoException extends RuntimeException{
-    public StatisticsInfoException(String message) {
-        super(message);
-    }
-}
